2fea68ebad851a1b3771637a9c9ad8d971e8571d
Ben Powell <powe0186@gmail.com>
2022-04-18 14:39:07 -0500

    Questions working now. Added recursive function to handle variable team sizes.

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
 let inquirer = require('inquirer');
 const createEmployee = require('./lib/createEmployee');
 let generateQs = require('./lib/generateQs');
-
+let employeeRoleQ = require('./lib/employeeRoleQ');
 let employeeList = [];
+
+const roleQuestion = [
+    {
+        type: 'list',
+        name: 'newRole',
+        message: 'What role is does your next employee have?',
+        choices: ['manager', 'engineer', 'intern', 'FINISHED', ]
+    }
+];
+
+function askAndCreate() {
+    inquirer
+    .prompt(roleQuestion)
+    .then((choice) =>{
+        const { newRole } = choice;
+        if (newRole === 'FINISHED') {
+            console.log("Building team....")
+            //Put code to create HTML HERE.
+        } else {
+            inquirer
+            .prompt(generateQs(newRole))
+            .then(answers => {
+                employeeList.push(createEmployee(answers, newRole));
+                console.log(employeeList);
+                askAndCreate();
+            })
+        }
+    })
+}
+
+
 init();
 
 
 function init() {
+
     console.log('Welcome to TEAM BUILDER!');
     console.log('Answer the following questions about your team.\n')
-    
-    askEmployeeQuestions('manager');
-    // generate and ask questins about the manager.
-    console.log(employeeList);
-    
-}
 
-//Generates and prompts the questions for the specific role passed into it.
-function askEmployeeQuestions(role) {
     inquirer
-        .prompt(generateQs(role))
-        .then((answers) => {
-            let employee = createEmployee(answers, role);
-            employeeList.push(employee);
-
-        })
+    .prompt(generateQs('manager'))
+    .then((answers) => {
+        employeeList.push(createEmployee(answers, 'manager'));
+        console.log(employeeList);
+    })
+    .then(() => {
+        askAndCreate();
+    })
 }
+
